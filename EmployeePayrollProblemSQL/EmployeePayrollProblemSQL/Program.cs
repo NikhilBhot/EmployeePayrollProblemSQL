ef459642b431ca64679cfca0596b578789fd6d00
unknown <bhotnikhil25@gmail.com>
2023-04-22 00:39:06 +0530

    [Nikhil]-Wel-Come TO EmployeePayRoll sql

---
+++ EmployeePayrollProblemSQL/EmployeePayrollProblemSQL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollProblemSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Problem!");
+        }
+    }
+}
